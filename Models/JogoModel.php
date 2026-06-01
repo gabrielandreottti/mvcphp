8d82e41fd482168d530870977ab2a615e3cea9bb
MICRO12 <emanuelle.saito@preve.g12.br>
2026-06-01 14:16:03 -0300

    first commit

---
+++ Models/JogoModel.php
@@ -0,0 +1,24 @@
+<?php
+class JogosModel 
+{
+        private $conexao;
+ public function __construct($conexao) {
+    $this-> $conexao = $conexao;
+    }
+    public function ListarJogos(): void {
+    //codigo de listar 
+    }
+    public function Buscarjogos(): void {
+    //codigo de busca
+    }
+    public function Adicionarjogos(): void {
+    //codigo de inserçao
+    }
+    public function Atualizarjogos(): void {
+    //codigo de atualizaçao
+    }
+    public function Deletarjogos(): void {
+    //codigo de deletar 
+    }
+}
+
